b0a8dc93562e013ebf14c563f00b73882c5d54a5
Steven Barragan <me@steven.mx>
2023-03-05 17:50:37 -0600

    feat: setup cropper when editing model

--- app/assets/javascripts/croppable.js
+++ app/assets/javascripts/croppable.js
@@ -1,14 +1,35 @@
 import Cropper from 'cropperjs';
 
 const input = document.querySelector('.croppable-input');
+const image = document.querySelector('.croppable-image');
 
 if (input) {
-  input.addEventListener('change', updateImageDisplay);
+  input.addEventListener('change', setupCropperFromInput);
 }
 
-function updateImageDisplay(event) {
+if (image) {
+  setupCropperFromImage(image)
+}
+
+function setupCropperFromInput(event) {
   const input      = event.target;
   const wrapper    = input.closest(".croppable-wrapper");
+
+  const file  = input.files[0];
+  const image = document.createElement('img');
+
+  image.src = URL.createObjectURL(file);
+
+  updateImageDisplay(image, wrapper, true)
+}
+
+function setupCropperFromImage(image) {
+  const wrapper = image.closest(".croppable-wrapper");
+
+  updateImageDisplay(image, wrapper, false)
+}
+
+function updateImageDisplay(image, wrapper, isNewImage) {
   const controls   = wrapper.querySelector(".croppable-controls");
   const container  = wrapper.querySelector(".croppable-container");
   const centerBtn  = wrapper.querySelector(".croppable-center");
@@ -17,14 +38,12 @@ function updateImageDisplay(event) {
   const xInput     = wrapper.querySelector(".croppable-x");
   const yInput     = wrapper.querySelector(".croppable-y");
   const scaleInput = wrapper.querySelector(".croppable-scale");
+  const width      = wrapper.dataset.width;
+  const height     = wrapper.dataset.height;
 
-  const width      = input.dataset.width;
-  const height     = input.dataset.height;
-
-  const file  = input.files[0];
-  const image = document.createElement('img');
-
-  image.src = URL.createObjectURL(file);
+  while(container.firstChild) {
+    container.removeChild(container.lastChild);
+  }
 
   const cropper = new Cropper(image, {container, template: template(width, height)});
 
@@ -33,25 +52,41 @@ function updateImageDisplay(event) {
 
   controls.style.display = "flex";
 
-  const matrix     = cropperImage.$getTransform();
-  xInput.value     = matrix[4];
-  yInput.value     = matrix[5];
-  scaleInput.value = matrix[0];
+  var saveTransform = false;
 
-  cropperCanvas.style.backgroundColor = bgColorBtn.value;
+  cropperImage.$ready(() => {
+    if (xInput.value != "" && !isNewImage) {
+      setTimeout(() => {
+        cropperImage.$setTransform(+scaleInput.value, 0, 0, +scaleInput.value, +xInput.value, +yInput.value);
+
+        saveTransform = true;
+      })
+    } else {
+      const matrix     = cropperImage.$getTransform();
+      xInput.value     = matrix[4];
+      yInput.value     = matrix[5];
+      scaleInput.value = matrix[0];
+
+      saveTransform = true;
+    }
+  })
 
   cropperImage.addEventListener('transform', (event) => {
-    const matrix = event.detail.matrix;
-
-    xInput.value     = matrix[4];
-    yInput.value     = matrix[5];
-    scaleInput.value = matrix[0];
+    if(saveTransform) {
+      const matrix = event.detail.matrix;
+      xInput.value     = matrix[4];
+      yInput.value     = matrix[5];
+      scaleInput.value = matrix[0];
+    }
   });
+
+  cropperCanvas.style.backgroundColor = bgColorBtn.value;
 
   bgColorBtn.addEventListener("change", (event) => {
     event.preventDefault();
     cropperCanvas.style.backgroundColor = event.target.value;
   })
+
 
   centerBtn.addEventListener("click", (event) => {
     event.preventDefault();
