53c6b4833f3401d0474316fe6b69ee8645569e9f
Steven Barragan <me@steven.mx>
2023-03-10 13:31:34 -0600

    refactor: open image directly from attribute
    * whithout calculating its URL
    * it avoids having to setup asset_host config

--- lib/croppable/crop.rb
+++ lib/croppable/crop.rb
@@ -2,45 +2,40 @@ require 'open-uri'
 require 'vips'
 
 module Croppable
-
   class Crop
     def initialize(model, attr_name)
       @model     = model
       @attr_name = attr_name
       @data      = model.send("#{attr_name}_croppable_data")
       @setup     = model.send("#{attr_name}_croppable_setup")
-      original   = model.send("#{attr_name}_original")
-      @url       = Rails.application.routes.url_helpers.rails_blob_url(original, host: Rails.application.config.asset_host)
     end
 
     def perform()
-      file = URI(@url).open
-      vips_img = Vips::Image.new_from_file(file.path)
+      @model.send("#{@attr_name}_original").open do |file|
+        vips_img = Vips::Image.new_from_file(file.path)
 
-      height = vips_img.height
-      width  = vips_img.width
+        height = vips_img.height
+        width  = vips_img.width
 
-      x = (width  - (width  * @data.scale)) / 2 + @data.x
-      y = (height - (height * @data.scale)) / 2 + @data.y
+        x = ((width  - (width  * @data.scale)) / 2 + @data.x) * @setup[:scale]
+        y = ((height - (height * @data.scale)) / 2 + @data.y) * @setup[:scale]
 
-      background = @data.background_color.remove("#").scan(/\w{2}/).map {|color| color.to_i(16) }
-      background_embed = background.dup
-      background_embed << 255 if vips_img.bands == 4
+        background = @data.background_color.remove("#").scan(/\w{2}/).map {|color| color.to_i(16) }
+        background_embed = background.dup
+        background_embed << 255 if vips_img.bands == 4
 
-      vips_img = vips_img.resize(@data.scale * @setup[:scale])
-      vips_img = vips_img.embed(
-        x * @setup[:scale],
-        y * @setup[:scale],
-        @setup[:width] * @setup[:scale],
-        @setup[:height] * @setup[:scale],
-        background: background_embed
-      )
+        new_width  = @setup[:width]  * @setup[:scale]
+        new_height = @setup[:height] * @setup[:scale]
 
-      path = Tempfile.new('cropped').path + ".jpg"
+        vips_img = vips_img.resize(@data.scale * @setup[:scale])
+        vips_img = vips_img.embed(x, y, new_width, new_height, background: background_embed)
 
-      vips_img.write_to_file(path, background: background, Q: 100)
+        path = Tempfile.new('cropped').path + ".jpg"
 
-      @model.send("#{ @attr_name }_cropped").attach(io: File.open(path), filename: "cropped")
+        vips_img.write_to_file(path, background: background, Q: 100)
+
+        @model.send("#{ @attr_name }_cropped").attach(io: File.open(path), filename: "cropped")
+      end
     end
   end
 end
