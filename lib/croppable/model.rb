c935d1bae7c11dc1bcb1d92e8601b85af8963d3c
Woody Peterson <woody.peterson@gmail.com>
2023-03-31 09:51:48 -0700

    Support Rails 7's named variants
    https://github.com/rails/rails/pull/39135
    
    https://edgeapi.rubyonrails.org/classes/ActiveStorage/Variant.html

--- lib/croppable/model.rb
+++ lib/croppable/model.rb
@@ -3,8 +3,8 @@ module Croppable
     extend ActiveSupport::Concern
 
     class_methods do
-      def has_croppable(name, width:, height:, scale: 1)
-        has_one_attached :"#{ name }_cropped"
+      def has_croppable(name, width:, height:, scale: 1, &block)
+        has_one_attached :"#{ name }_cropped", &block
         has_one_attached :"#{ name }_original"
 
         has_one :"#{ name }_croppable_data", -> { where(name: name) },
