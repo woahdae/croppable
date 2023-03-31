2563288eea5ab5ebdcb5ad09c585598e767a2e10
Woody Peterson <woody.peterson@gmail.com>
2023-03-31 05:23:15 -0700

    Set tests as default rake task
    Previously, it wasn't obvious how to get the tests to run, and
    attempting via rake produced missing table errors. This fixes both by
    adding tests as a task (the default rake task), and moving the
    croppable_data migration into the dummy app.

---
+++ test/dummy/db/migrate/20230328223705_create_croppable_data.croppable.rb
@@ -0,0 +1,15 @@
+# This migration comes from croppable (originally 20230303170333)
+class CreateCroppableData < ActiveRecord::Migration[7.0]
+  def change
+    create_table :croppable_data do |t|
+      t.references :croppable, polymorphic: true
+      t.string     :name
+      t.float      :scale
+      t.integer    :x
+      t.integer    :y
+      t.string     :background_color
+
+      t.timestamps
+    end
+  end
+end
