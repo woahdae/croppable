cbc24ed3be5c152cd0606e9695e0c6adf13bf871
Woody Peterson <woody.peterson@gmail.com>
2023-04-02 15:59:31 -0700

    MiniMagick support
    Even though VIPS is great, it's not available on all platforms, ex.
    Github CI. Let's just implement MiniMagick support to make our lives
    easier when we don't have VIPS.

--- lib/croppable/config.rb
+++ lib/croppable/config.rb
@@ -5,6 +5,14 @@ module Croppable
         Config.instance.tap do |config|
           config.default_scale = 1
           config.image_quality = 100
+
+          begin
+            require 'ruby-vips'
+            config.crop_backend = :vips
+          rescue LoadError
+            require 'mini_magick'
+            config.crop_backend = :mini_magick
+          end
         end
     end
   end
@@ -14,5 +22,13 @@ module Croppable
 
     attr_accessor :default_scale
     attr_accessor :image_quality
+    attr_accessor :crop_backend
+
+    def crop_backend=(value)
+      raise "Invalid crop backend: #{value}"\
+        unless %i[vips mini_magick].include?(value)
+
+      @crop_backend = value
+    end
   end
 end
--- lib/croppable/crop.rb
+++ lib/croppable/crop.rb
@@ -1,43 +1,25 @@
 require 'open-uri'
-require 'vips'
 
 module Croppable
   class Crop
-    def initialize(model, attr_name, uploaded_file: nil)
+    def backend
+      @backend || Croppable.config.crop_backend
+    end
+
+    def initialize(model, attr_name, uploaded_file: nil, backend: nil)
       @model         = model
       @attr_name     = attr_name
       @uploaded_file = uploaded_file || {}
+      @backend       = backend
       @data          = model.send("#{attr_name}_croppable_data")
       @setup         = model.send("#{attr_name}_croppable_setup")
     end
 
     def perform()
-      uploaded_file_or_original do |file|
-        vips_img = Vips::Image.new_from_file(file.path)
-
-        height = vips_img.height
-        width  = vips_img.width
-
-        x = ((width  - (width  * @data.scale)) / 2 + @data.x) * @setup[:scale]
-        y = ((height - (height * @data.scale)) / 2 + @data.y) * @setup[:scale]
-
-        background = @data.background_color.remove("#").scan(/\w{2}/).map {|color| color.to_i(16) }
-        background_embed = background.dup
-        background_embed << 255 if vips_img.bands == 4
-
-        new_width  = @setup[:width]  * @setup[:scale]
-        new_height = @setup[:height] * @setup[:scale]
-
-        vips_img = vips_img.resize(@data.scale * @setup[:scale])
-        vips_img = vips_img.embed(x, y, new_width, new_height, background: background_embed)
-
-        path = Tempfile.new('cropped').path + ".jpg"
-
-        vips_img.write_to_file(path, background: background, Q: Croppable.config.image_quality)
-
-        @model.send("#{ @attr_name }_cropped").attach(io: File.open(path), filename: "cropped")
-      end
+      uploaded_file_or_original { |file| send("process_with_#{backend}", file) }
     end
+
+    private
 
     # When performing an inline job, such as in a test or by choice in an app,
     # we only have the uploaded file; the original will not be written to
@@ -53,6 +35,68 @@ module Croppable
       else
         @model.send("#{@attr_name}_original").open(&block)
       end
+    end
+
+    def process_with_vips(file)
+      img = Vips::Image.new_from_file(file.path)
+
+      x, y = *offsets(width: img.width, height: img.height)
+
+      bg_rgb = background_rgb
+      bg_rgb << 255 if img.bands == 4
+
+      img = img.resize(crop_scale)
+      img = img.embed(x, y, new_width, new_height, background: bg_rgb)
+
+      path = Tempfile.new('cropped').path + ".jpg"
+
+      img.write_to_file(path, background: background_rgb, Q: Croppable.config.image_quality)
+
+      @model.send("#{ @attr_name }_cropped").attach(io: File.open(path), filename: "cropped")
+    end
+
+    def process_with_mini_magick(file)
+      img = MiniMagick::Image.open(file.path)
+
+      x, y = *offsets(width: img.width, height: img.height)
+      x = x.negative? ? "+#{x}" : "-#{x}"
+      y = y.negative? ? "+#{y}" : "-#{y}"
+
+      img.format('jpg')
+      img.scale("#{crop_scale * 100}%")
+      img.combine_options do |opts|
+        opts.background(@data.background_color)
+        opts.extent("#{new_width}x#{new_height}#{x}#{y}")
+      end
+
+      @model.send("#{ @attr_name }_cropped").attach(io: File.open(img.path), filename: "cropped")
+    end
+
+    def offsets(width:, height:)
+      [ offset_for(:x, width), offset_for(:y, height) ]
+    end
+
+    def offset_for(axis, length)
+      ((length  - (length  * @data.scale)) / 2 + @data.send(axis)) * @setup[:scale]
+    end
+
+    def new_width
+      @setup[:width] * @setup[:scale]
+    end
+
+    def new_height
+      @setup[:height] * @setup[:scale]
+    end
+
+    def crop_scale
+      @data.scale * @setup[:scale]
+    end
+
+    def background_rgb
+      @data.background_color
+        .remove("#")
+        .scan(/\w{2}/)
+        .map {|color| color.to_i(16) }
     end
   end
 end
