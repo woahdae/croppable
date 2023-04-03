9be278631d9e8f74294022265e42c37ce467d888
Woody Peterson <woody.peterson@gmail.com>
2023-04-03 11:33:32 -0700

    Fix race condition on cropping uploaded files
    Previously, if the file hadn't finished uploading to storage when the
    crop job started, the job would fail with a
    ActiveStorage::FileNotFoundError.
    
    This sets the job to retry every second for 5 seconds.

--- app/jobs/croppable/crop_image_job.rb
+++ app/jobs/croppable/crop_image_job.rb
@@ -3,6 +3,9 @@ require "croppable/crop"
 module Croppable
   class CropImageJob < ApplicationJob
     queue_as :default
+    # Sometimes the file hasn't been uploaded to storage when the job starts.
+    # Default is 3s delay and 5 attempts, we'll do 1s delay & default attempts.
+    retry_on ActiveStorage::FileNotFoundError, wait: 1
 
     def perform(model, croppable_name, uploaded_file: nil)
       Croppable::Crop.new(
